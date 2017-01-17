70beadb3bc4d2d57692ed5022261458dcae7b610
DHaylock <david.haylock@watershed.co.uk>
2017-01-17 14:57:29 +0000

    - Added KinectManager to ofApp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,71 +1,56 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
-
+void ofApp::setup()
+{
+	ofSetLogLevel(OF_LOG_VERBOSE);
+	ofSetFrameRate(30);
+	ofBackground(0);
+	
+	kinectManager.setup();
+	kinectManager.setFarThreshold(128);
+	kinectManager.setNearThreshold(255);
 }
-
 //--------------------------------------------------------------
-void ofApp::update(){
-
+void ofApp::update()
+{
+	kinectManager.update();
 }
-
 //--------------------------------------------------------------
-void ofApp::draw(){
-
+void ofApp::draw()
+{
+	kinectManager.draw();
 }
-
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::keyPressed(int key)
+{
+	switch(key) {
+		case '<':
+			kinectManager.decreaseFarThreshold();
+			break;
+		case ',':
+			kinectManager.decreaseNearThreshold();
+			break;
+		case '>':
+			kinectManager.increaseFarThreshold();
+			break;
+		case '.':
+			kinectManager.increaseNearThreshold();
+			break;
+		case 'r':
+			kinectManager.resetModel();
+			break;
+		default:
+			break;
+	}
 }
-
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::keyReleased(int key)
+{
+	
 }
-
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::exit()
+{
+	kinectManager.exit();
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "KinectManager.h"
 
 class ofApp : public ofBaseApp{
 
@@ -8,17 +9,11 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
-
+		void exit();
+	
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	
+		KinectManager kinectManager;
+	
 };
